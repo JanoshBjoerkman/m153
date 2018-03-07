c5f492dd58b42adeeedfd06d7cc2b525b03c5eae
Janosh Björkman <janosh.bbluewin.ch>
2018-03-07 08:45:01 +0100

    init

---
+++ hierarchical/model/post.php
@@ -0,0 +1,35 @@
+<?php
+
+class Post {
+
+  public $id;
+  public $postingDate;
+  public $content;
+  
+  public $subPosts;
+  public $previousPost;
+
+  //user data
+  public $userId;
+  public $firstname;
+  public $lastname;
+  public $avatar;
+
+
+  public function __construct ($id, $postingDate, $content, $userId, $firstname, $lastname, $avatar) {
+
+    $this->id = $id;
+    $this->postingDate = new DateTime ($postingDate);
+    $this->content = $content;
+
+    $this->userId = $userId;
+    $this->firstname = $firstname;
+    $this->lastname = $lastname;
+    $this->avatar = $avatar;
+
+  }
+
+
+}
+
+ ?>
